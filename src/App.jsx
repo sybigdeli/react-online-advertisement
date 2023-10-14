710ccf9dbb9d3198c19d2f2814b8f856a733f06e
Seyyed Sadrodin Bigdeli <sadra.bigdeli1350@gmail.com>
2023-10-14 19:39:47 +0330

    test tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 function App() {
   return (
     <>
-      <div className="text">
-        <div className="w-20 h-20 bg-red-500 rounded-full flex items-center justify-center text-white p-10">
+      <div className="flex justify-center items-center min-h-screen">
+        <div className="w-20 h-20 bg-red-500 rounded-full flex items-center justify-center text-white p-14 border-4 border-black transition hover:scale-150 hover:bg-red-700 cursor-pointer">
           OnlineWeb
         </div>
       </div>
