dd2e92f06e9366ce1bd6a3bab3e644405965591b
amir momen <amirmomen.call@gmail.com>
2023-10-16 19:53:22 +0330

    Update btn

--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -1,7 +1,8 @@
 const Button = ({ children, variant, ...props }) => {
   const variants = {
-    fill: "w-[150px] h-[42px] rounded-[20px] bg-primary-500 text-white text-[16px] font-semibold leading-4",
-    outline: "w-[120px] h-[42px] rounded-[30px] border border-primary-500 text-primary-500 text-[16px] font-semibold leading-4 hover:bg-primary-500",
+    fill: "w-[150px] text-center h-[42px] rounded-[20px] bg-primary-500 text-white text-[16px] font-semibold leading-4",
+    outline:
+      "w-[120px] text-center h-[42px] rounded-[30px] border border-primary-500 text-primary-500 text-[16px] font-semibold leading-4  hover:text-white hover:bg-primary-500 transition-all",
   };
 
   return (
