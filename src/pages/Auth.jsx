e5495b8fb8287d5d53968d1bd42757263e5f785f
Seyyed Sadrodin Bigdeli <sadra.bigdeli1350@gmail.com>
2023-10-16 18:18:32 +0330

    UI components

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
+import Button from "../components/UI/Button";
 
 const Auth = () => {
   return (
     <div>
       <Link to="/">صفحه اول</Link>
       <h1>ورود / عضویت</h1>
+      <Button>ورود</Button>
     </div>
   );
 };
