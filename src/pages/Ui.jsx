e8cdbe5ca6e9a6d934ccd4d427520bd1e6280600
Seyyed Sadrodin Bigdeli <sadra.bigdeli1350@gmail.com>
2023-10-18 17:21:19 +0330

    add badges

--- src/pages/Ui.jsx
+++ src/pages/Ui.jsx
@@ -1,9 +1,13 @@
 import Button from "../components/UI/Button";
+import AppleBadge from '../assets/icons/apple-app-store-badge 1.svg'
+import GoogleBadge from '../assets/icons/Google_Play_Store_badge_EN 1.svg'
 function Ui() {
   return (
     <div>
       <Button variant="fill">ورود</Button>
       <Button variant="outline">ورود</Button>
+      <img src={AppleBadge} />
+      <img src={GoogleBadge} />
     </div>
   );
 }
