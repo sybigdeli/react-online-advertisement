ded4251ec50985a0e340bb9871c182187f6a9ccd
Seyyed Sadrodin Bigdeli <sadra.bigdeli1350@gmail.com>
2023-10-18 17:59:31 +0330

    add logo

--- src/pages/Ui.jsx
+++ src/pages/Ui.jsx
@@ -2,8 +2,9 @@ import Button from "../components/UI/Button";
 import AppleBadge from "../assets/icons/apple-app-store-badge 1.svg";
 import GoogleBadge from "../assets/icons/Google_Play_Store_badge_EN 1.svg";
 import CategoryRow from "../components/UI/CategoryRow";
-import CommunityImage from "../assets/icons/Group.svg"
+import CommunityImage from "../assets/icons/Group.svg";
 import Tags from "../components/UI/Tags";
+import Logo from "../components/UI/Logo";
 function Ui() {
   return (
     <div>
@@ -11,10 +12,11 @@ function Ui() {
       <Button variant="outline">ورود</Button>
       <img src={AppleBadge} />
       <img src={GoogleBadge} />
-      <CategoryRow image={CommunityImage} text="Community"/>
+      <CategoryRow image={CommunityImage} text="Community" />
       <div className="w-[124px] h-9">
         <Tags tagName={"Mountain Bikes"} />
       </div>
+      <Logo />
     </div>
   );
 }
