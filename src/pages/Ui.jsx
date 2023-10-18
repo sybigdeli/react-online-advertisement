75844e89bd44e920a157035a67ac78c1ab15a819
Mahdi Nasiban <m.nasiban@yahoo.com>
2023-10-18 17:37:58 +0330

    tag component replaced

--- src/pages/Ui.jsx
+++ src/pages/Ui.jsx
@@ -1,6 +1,7 @@
 import Button from "../components/UI/Button";
-import AppleBadge from '../assets/icons/apple-app-store-badge 1.svg'
-import GoogleBadge from '../assets/icons/Google_Play_Store_badge_EN 1.svg'
+import AppleBadge from "../assets/icons/apple-app-store-badge 1.svg";
+import GoogleBadge from "../assets/icons/Google_Play_Store_badge_EN 1.svg";
+import Tags from "../components/UI/Tags";
 function Ui() {
   return (
     <div>
@@ -8,6 +9,9 @@ function Ui() {
       <Button variant="outline">ورود</Button>
       <img src={AppleBadge} />
       <img src={GoogleBadge} />
+      <div className="w-[124px] h-9">
+        <Tags tagName={"Mountain Bikes"} />
+      </div>
     </div>
   );
 }
