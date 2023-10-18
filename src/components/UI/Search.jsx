12b8e15be88bfa94c9260b8b6660db58db568a82
amir momen <amirmomen.call@gmail.com>
2023-10-18 19:31:10 +0330

    finished search

--- src/components/UI/Search.jsx
+++ src/components/UI/Search.jsx
@@ -3,18 +3,18 @@ import DownIcon from "../../assets/icons/down.svg";
 
 function Search({ children, varient }) {
   return (
-    <div className="searchParent w-96 h-12">
-      <div className="search w-full h-full rounded-2xl bg-slate-400 flex justify-between items-center">
+    <div className="searchParent w-96 h-12 ">
+      <div className="search  w-full h-full px-[10px] rounded-[20px] bg-white border-solid border-2 border-neutral-200 flex justify-between items-center">
+        <div className="search-category min-w-min flex justify-between ">
+          <span className="">{children}</span>
+          <img src={DownIcon} alt="" />
+          <div className="w-[1px] self-stretch bg-slate-200 rounded-[30px]"></div>
+        </div>
         <input
           type="text"
           value={varient}
           className="bg-transparent outline-0"
         />
-
-        <div className="search-category min-w-min flex justify-between">
-          <span>{children}</span>
-          <img src={DownIcon} alt="" />
-        </div>
 
         <img src={SearchIcon} />
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    fontFamily:{
-sans: ["Estedad"],
-serif:["Estedad"],
-
+    fontFamily: {
+      sans: ["Estedad"],
+      serif: ["Estedad"],
     },
     extend: {
       colors: {
         primary: {
-          200:"#B160FB",
+          200: "#B160FB",
           500: "#B160FB",
         },
       },
