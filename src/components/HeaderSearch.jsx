c271609d855f442318720652b92effca4d10a14e
amir momen <amirmomen.call@gmail.com>
2023-10-25 19:32:52 +0330

    UPDATE...

--- src/components/HeaderSearch.jsx
+++ src/components/HeaderSearch.jsx
@@ -4,9 +4,14 @@ import DownImage from "../assets/icons/down.svg";
 import Search from "./UI/Search";
 import MapPin from "../assets/icons/map-pin.svg";
 import ProfileImage from "../assets/images/ProfileImage.svg";
-function HeaderSearch() {
+function HeaderSearch({ onClick }) {
   return (
     <div className="w-full flex justify-between items-center gap-10 px-[60px] py-[30px]">
+      <div
+        className="w-10 h-10 bg-slate-300 rounded-md lg:hidden"
+        onClick={onClick}
+      ></div>
+
       <div className="flex flex-row items-center justify-center gap-[20px]">
         <div className="flex flex-row items-center justify-center gap-2">
           <img
@@ -18,11 +23,9 @@ function HeaderSearch() {
           </p>
           <img src={ProfileImage} className="w-full h-full" />
         </div>
-
         <Button variant="fill">Create a Post</Button>
       </div>
-
-      <div className="flex flex-row items-center justify-end flex-grow gap-[20px]">
+      <div className="hidden lg:flex flex-row items-center justify-end flex-grow gap-[20px]">
         <div className="flex-grow">
           <Search>Bicycle</Search>
         </div>
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,39 +2,72 @@ import HeaderSearch from "@/components/HeaderSearch";
 import ProductBox from "@/components/Product/ProductBox";
 import Footer from "@/components/Footer";
 import { Link } from "react-router-dom";
+import Rectangle from "@/components/UI/Rectangle";
+import { useEffect, useState } from "react";
 
 const Search = () => {
+  const [click, SetClick] = useState(false);
+
+  const handleClick = () => {
+    SetClick(true);
+  };
+
+  useEffect(() => {
+    const handleClick = (event) => {
+      const isClickInForm = event.target.closest(".rectangle");
+      // console.log(isClickInForm);
+      if (!isClickInForm) {
+        SetClick(false);
+      }
+    };
+    if (click) {
+      setTimeout(() => {
+        document.body.addEventListener("click", handleClick);
+      }, 0);
+    }
+    return () => {
+      document.body.removeEventListener("click", handleClick);
+    };
+  }, [click]);
+
   return (
-    <div>
+    <>
+      <>
+        {click && (
+          <div className="rectangle  fixed top-0 right-0 w-1/3 h-full z-10">
+            <Rectangle></Rectangle>
+          </div>
+        )}
+      </>
       <div>
-        <HeaderSearch></HeaderSearch>
-      </div>
-      <div className="grid grid-cols-5 gap-8">
-        <div className="search-content-right col-span-1 border-l-[1px] flex justify-center">
-          <div className="bg-slate-100 w-5/6 h-full  rounded-md text-[20px] flex justify-center items-center">
-            در دست توسعه
+        <div>
+          <HeaderSearch onClick={handleClick}></HeaderSearch>
+        </div>
+        <div className="grid grid-cols-5 gap-8">
+          <div className="col-span-1 border-l-[1px] hidden lg:flex">
+            <Rectangle></Rectangle>
+          </div>
+          <div className="search-content-left grid  col-span-5 lg:col-span-4 ">
+            <div className="result-section bg-slate-100 w-full h-[100px] rounded-lg text-[20px] flex justify-center items-center">
+              در دست توسعه
+            </div>
+
+            <div className="grid grid-cols-2 sm:gap-16 md:grid-cols-3 md:gap-4 lg:grid-cols-3 lg:gap-1 xl:grid-cols-4 xl:gap-2">
+              <ProductBox>در دست توسعه</ProductBox>
+              <ProductBox>در دست توسعه</ProductBox>
+              <ProductBox>در دست توسعه</ProductBox>
+              <ProductBox>در دست توسعه</ProductBox>
+              <ProductBox>در دست توسعه</ProductBox>
+              <ProductBox>در دست توسعه</ProductBox>
+              <ProductBox>در دست توسعه</ProductBox>
+              <ProductBox>در دست توسعه</ProductBox>
+            </div>
           </div>
         </div>
-        <div className="search-content-left grid  col-span-4 ">
-          <div className="result-section bg-slate-100 w-full h-[100px] rounded-lg text-[20px] flex justify-center items-center">
-            در دست توسعه
-          </div>
 
-          <div className="grid grid-cols-4 gap-4">
-            <ProductBox>در دست توسعه</ProductBox>
-            <ProductBox>در دست توسعه</ProductBox>
-            <ProductBox>در دست توسعه</ProductBox>
-            <ProductBox>در دست توسعه</ProductBox>
-            <ProductBox>در دست توسعه</ProductBox>
-            <ProductBox>در دست توسعه</ProductBox>
-            <ProductBox>در دست توسعه</ProductBox>
-            <ProductBox>در دست توسعه</ProductBox>
-          </div>
-        </div>
+        <Footer></Footer>
       </div>
-
-      <Footer></Footer>
-    </div>
+    </>
   );
 };
 
