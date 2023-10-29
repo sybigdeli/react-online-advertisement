0a3d6d0824d8bcecb97164dd42fe4da98c0973dd
Seyyed Sadrodin Bigdeli <sadra.bigdeli1350@gmail.com>
2023-10-29 17:55:49 +0330

    add sign up and validation

---
+++ src/components/Auth/LoginForm.jsx
@@ -0,0 +1,52 @@
+import Button from "@/components/UI/Button";
+import FormLogin from "@/components/UI/FormLogin";
+import { useState } from "react";
+
+function LoginForm() {
+  const [loginForm, setLoginForm] = useState({
+    username: "",
+    password: "",
+  });
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(loginForm);
+  };
+
+  const handleChangeLoginForm = (e , key) => {
+    setLoginForm((s) => ({...s , [key] : e.target.value}))
+  }
+  return (
+    <form
+      onSubmit={(event) => handleSubmit}
+      className="w-full max-w-md shadow flex flex-col justify-center items-center gap-6 px-6 py-12"
+    >
+      <h4 className="text-center text-zinc-800 text-xl font-bold">ورود</h4>
+      <FormLogin
+        inputName="user-name"
+        inputType="text"
+        label="نام کاربری"
+        placeHolder="نام کاربری خود را وارد کنید"
+        value={loginForm.username}
+        onChange={(e) => handleChangeLoginForm(e, "username")}
+      />
+      <FormLogin
+        inputName="user-pass"
+        inputType="password"
+        label="رمز عبور"
+        placeHolder="رمز عبور خود را وارد کنید"
+        value={loginForm.password}
+        onChange={(e) => handleChangeLoginForm(e, "password")}
+      />
+      <div className="w-full flex items-start">
+        <p className="text-center text-zinc-800 text-sm font-bold">
+          رمز عبور خود را فراموش کرده اید؟
+        </p>
+      </div>
+      <Button variant="fill" onClick={handleSubmit}>
+        ورود
+      </Button>
+    </form>
+  );
+}
+
+export default LoginForm;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,62 +1,35 @@
 import LoginImageDesktop from "@/assets/images/LoginImage.png";
 import LoginImageMobile from "@/assets/images/LoginImageMobile.png";
+import LoginForm from "@/components/Auth/LoginForm";
+import RegisterForm from "@/components/Auth/RegisterForm";
 import Button from "@/components/UI/Button";
 import FormLogin from "@/components/UI/FormLogin";
 import Logo from "@/components/UI/Logo";
 import { useState } from "react";
 const Login = () => {
-  const [loginForm, setLoginForm] = useState({
-    username: "",
-    password: "",
-  });
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(loginForm);
-  };
-
-  const handleChangeLoginForm = (e , key) => {
-    setLoginForm((s) => ({...s , [key] : e.target.value}))
-  }
+  const [mode, setMode] = useState("login");
   return (
     <div className="flex flex-col-reverse justify-end h-screen gap-8 md:flex-row md:justify-center md:items-center md:p-8 md:overflow-hidden">
       <div className="basis-1/2 rounded-lg flex flex-col flex-grow justify-center items-center gap-8 px-4">
         <div className="w-full flex justify-center items-center">
           <Logo />
         </div>
-        <form
-          onSubmit={(event) => handleSubmit}
-          className="w-full max-w-md shadow flex flex-col justify-center items-center gap-6 px-6 py-12"
-        >
-          <h4 className="text-center text-zinc-800 text-xl font-bold">ورود</h4>
-          <FormLogin
-            inputName="user-name"
-            inputType="text"
-            label="نام کاربری"
-            placeHolder="نام کاربری خود را وارد کنید"
-            value={loginForm.username}
-            onChange={(e) => handleChangeLoginForm(e , "username")}
-          />
-          <FormLogin
-            inputName="user-pass"
-            inputType="password"
-            label="رمز عبور"
-            placeHolder="رمز عبور خود را وارد کنید"
-            value={loginForm.password}
-            onChange={(e) => handleChangeLoginForm(e , "password")}
-          />
-          <div className="w-full flex items-start">
-            <p className="text-center text-zinc-800 text-sm font-bold">
-              رمز عبور خود را فراموش کرده اید؟
-            </p>
-          </div>
-          <Button variant="fill" onClick={handleSubmit}>
-            ورود
-          </Button>
+        {mode === "login" ? <LoginForm /> : <RegisterForm />}
+        {mode === "login" ? (
           <div className="flex flex-warp justify-center items-center gap-1.5">
             <p className="text-center">حساب کاربری ندارید ؟</p>
-            <p className="text-center">ایجاد حساب کاربری</p>
+            <button className="text-center" onClick={() => (setMode("signUp"))}>
+              ایجاد حساب کاربری
+            </button>
           </div>
-        </form>
+        ) : (
+          <div className="flex flex-warp justify-center items-center gap-1.5">
+            <p className="text-center">حساب کاربری دارید ؟</p>
+            <button className="text-center" onClick={() => (setMode("login"))}>
+              ورود کنید
+            </button>
+          </div>
+        )}
       </div>
 
       <div className="basis-1/2 self-stretch relative rounded-3xl overflow-hidden">
