d176457afd555de7a923bccd965156f82ece3da2
amir momen <amirmomen.call@gmail.com>
2023-11-15 16:52:45 +0330

    aaa

---
+++ src/components/Side/Filter.jsx
@@ -0,0 +1,44 @@
+import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
+import Slider from "@mui/material/Slider";
+import Input from "@mui/material/Input";
+
+import { useState } from "react";
+
+function Filter() {
+  const [value, setValue] = useState([0, 69000000]);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const handleInputChange = (index, event) => {
+    const newValues = [...value];
+    newValues[index] =
+      event.target.value === "" ? 0 : Number(event.target.value);
+    setValue(newValues);
+  };
+
+  return (
+    <Box sx={{}}>
+      <Stack>
+        <Slider
+          value={value[1]}
+          onChange={handleChange}
+          min={0}
+          max={69000000}
+          step={10000}
+          valueLabelDisplay="auto"
+          aria-labelledby="range-slider"
+          getAriaValueText={(value) => `${value}`}
+        />
+      </Stack>
+      <Stack direction="row">
+        <Input value={value[1]} onChange={(e) => handleInputChange(1, e)} />
+        <Input value={value[0]} onChange={(e) => handleInputChange(0, e)} />
+      </Stack>
+    </Box>
+  );
+}
+
+export default Filter;
